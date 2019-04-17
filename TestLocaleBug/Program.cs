e93ba655eadfcc329b87f2911e4a5d9ff3202698
Mark Junker <mark.junker@ventuno.de>
2019-04-17 09:28:31 +0200

    Projektdateien hinzufügen.

---
+++ TestLocaleBug/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+
+namespace TestLocaleBug
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            TestHashSetComparison(StringComparer.OrdinalIgnoreCase);
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfoByIetfLanguageTag("tr-TR");
+            TestHashSetComparison(StringComparer.OrdinalIgnoreCase);
+        }
+
+        private static void TestHashSetComparison(IEqualityComparer<string> comparer)
+        {
+            Console.WriteLine(Thread.CurrentThread.CurrentCulture);
+
+            var set = new HashSet<string>(comparer)
+            {
+                "A",
+            };
+
+            if (!set.Contains("a"))
+            {
+                throw new InvalidProgramException();
+            }
+        }
+    }
+}
